6d3233961de25f306eefe2d77864b25e820073d2
J. Christian Romero <jcromerohdz@gmail.com>
2020-03-25 17:44:50 -0700

    I'm getting an error when I call the youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
 
 class App extends React.Component {
+  
+
+  onTermSubmit = async (term) => {
+    const KEY  = 'AIzaSyDnoVIpKAbnjgKcU7-f0jQdqMrl13tz7GM';
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: 'video',
+        key: KEY
+      }
+    });
+
+    console.log(term);
+    
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
       </div>
     );
   }
