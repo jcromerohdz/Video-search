961a95cf0f19890f455c9f89b0e5229f05c6613c
J. Christian Romero <jcromerohdz@gmail.com>
2020-03-30 13:28:06 -0700

    fetching data from youtube v3 api and listed in a video item also styling the video item compontent

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 
 class App extends React.Component {
   
+  state = { videos: [] };
 
   onTermSubmit = async (term) => {
-    const KEY  = 'AIzaSyDnoVIpKAbnjgKcU7-f0jQdqMrl13tz7GM';
+    //const KEY = "AIzaSyBXAyimeyDQaq0xckMpjEG4NzYGJR_XTbg";
+    const KEY = "AIzaSyA9tHarzPMsBCdwtsgrgwFVxhCYZC2HKIc";
+    // const KEY = "AIzaSyB50IXyJcf1a5TEGNjaf1-s06qjLYhKkSA";
     const response = await youtube.get("/search", {
       params: {
         q: term,
@@ -18,14 +22,16 @@ class App extends React.Component {
       }
     });
 
-    console.log(term);
+    this.setState({ videos: response.data.items });
+    
     
   }
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
